caf49ae0b54cd177839b6d572e48b6b66f857ccb
Vlad Tsyrklevich <vlad@tsyrklevich.net>
2016-10-31 12:05:16 +0100

    Update isInLoop/reachableCfgNodes to work with gremlin3

--- projects/languages/joern-lang/src/main/groovy/cfg.groovy
+++ projects/languages/joern-lang/src/main/groovy/cfg.groovy
@@ -15,20 +15,25 @@ addStep("toExitNode", {
     the entire search rather than just along the current path (massive optimization for
     high branching-factor CFGs, e.g. state machines).
 */
-_reachableCfgNodes = { def args ->
-
-  def curNodes = args[0]; def visited = args[1];
-  nextNodes = curNodes._().out('FLOWS_TO').toSet() - visited
+_reachableCfgNodes = { curNodes, visited, forward ->
+  if (forward == true) {
+    nextNodes = g.V(*curNodes).out(CFG_EDGE).toSet() - visited
+  } else {
+    nextNodes = g.V(*curNodes).in(CFG_EDGE).toSet() - visited
+  }
   if (nextNodes.isEmpty()) { return visited }
 
   visited.addAll(nextNodes)
-  return _reachableCfgNodes(nextNodes.toList(), visited)
+  return _reachableCfgNodes(nextNodes.toList(), visited, forward)
 }
 
-reachableCfgNodes = {
-  _().transform { _reachableCfgNodes(it.statements().toList(), new HashSet())}.scatter()
-}
+/* Finds all nodes in the CFG forwards or backwards reachable from the current node */
+addStep('reachableCfgNodes', { args_list ->
+  def forward = args_list[0]
+  g.V(*_reachableCfgNodes(delegate.statements().toList(), new HashSet(), forward).toList())
+})
 
 isInLoop = { it ->
-  it._().reachableCfgNodes().toSet().contains(it.statements().toList()[0])
+  statement = it.statements().toList()
+  _reachableCfgNodes(statement, new HashSet(), true).contains(statement[0])
 }
